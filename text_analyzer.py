c28a62b77eff3aa8af9f80a114eec1dcff11c08d
Amirreza <bamirreza5@gmail.com>
2024-11-11 20:23:26 +0330

    add number of sentences

--- text_analyzer.py
+++ text_analyzer.py
@@ -11,4 +11,14 @@ text = read_file('./index.txt')
 
 lines = text.splitlines()
 num_lines = len(lines)
-print(num_lines)
+print("number of lines : ", num_lines)
+
+"""پیاده سازی برای شمارش جملات"""
+sentences = []
+start = 0
+for i, char in enumerate(text):
+    if char in ".!?":
+        sentences.append(text[start:i+1].strip())
+        start = i + 1
+num_sentences = len(sentences)
+print("Number of sentences:", num_sentences)
