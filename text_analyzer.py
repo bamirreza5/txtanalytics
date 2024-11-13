a4a424a52b59566d9d1be27d2bb11bbd7e531211
Amirreza <bamirreza5@gmail.com>
2024-11-13 17:37:12 +0330

    add input and output , ignored input file

--- text_analyzer.py
+++ text_analyzer.py
@@ -24,8 +24,18 @@ def count_words(text):
     words = [word.strip(".,!?;:") for word in text.split()]
     return len(words), words
 
-def filter_words(words, min_length, max_length):
-    return [word for word in words if min_length <= len(word) <= max_length]
+def read_ignored_words(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            print(file)
+            return set(word.strip() for word in file.readlines())
+    except FileNotFoundError:
+        print(f"Ignored words file not found: {file_path}")
+        return set()
+
+def filter_words(words, min_length, max_length, ignored_words):
+    return [word for word in words if min_length <= len(word) <= max_length and word.lower() not in ignored_words]
+
     # Using list comprehension for simplicity and efficiency
     # result = []
     # for word in words:
@@ -33,8 +43,6 @@ def filter_words(words, min_length, max_length):
     #         result.append(word)
     # return result
     
-
-
 def generate_word_combinations(filtered_words, consecutive_count):
     combinations = {}
     for i in range(len(filtered_words) - consecutive_count + 1):
@@ -63,27 +71,29 @@ def combine_results(output_dir):
             final_content += file.read()
     save_to_file(os.path.join(output_dir, 'output.txt'), final_content)
 
-def main(input_file, output_dir, min_length=4, max_length=4, consecutive_words=3):
+def main(input_file, output_dir, ignored_words_file, min_length=4, max_length=4, consecutive_words=3):
     text = read_file(input_file)
     if text is None:
         return
 
+    ignored_words = read_ignored_words(ignored_words_file)
+    
     num_lines = count_lines(text)
     num_sentences = count_sentences(text)
     num_words, words = count_words(text)
-    filtered_words = filter_words(words, min_length, max_length)
+    filtered_words = filter_words(words, min_length, max_length, ignored_words)
     word_combinations = generate_word_combinations(filtered_words, consecutive_words)
 
     save_results(output_dir, num_lines, num_sentences, num_words, word_combinations)
-
     combine_results(output_dir)
 
+
 if __name__ == "__main__":
-    # input_file = './index.txt' 
-    input_file = input('Enter input directory : ')
-    output_dir = input('Enter output directory : ')
+    input_file = input('Enter input directory: ')
+    output_dir = input('Enter output directory: ')
+    ignored_words_file = input('Enter ignored words file path: ')
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    main(input_file, output_dir)
+    main(input_file, output_dir, ignored_words_file)
